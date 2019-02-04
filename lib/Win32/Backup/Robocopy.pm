22fd5ab91906b8ca15db0a48055564addf6b1239
LorenzoTa <LorenzoTa@users.noreply.github.com>
2019-02-04 18:36:20 +0100

    version 11

--- lib/Win32/Backup/Robocopy.pm
+++ lib/Win32/Backup/Robocopy.pm
@@ -11,7 +11,7 @@ use JSON::PP; # only this support sort_by(custom_func)
 use Capture::Tiny qw(capture);
 use Algorithm::Cron;
 
-our $VERSION = 10;
+our $VERSION = 11;
 
 sub new {
 	my $class = shift;
